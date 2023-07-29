a22651a010b44d3faf9fde6f39168514330b6104
rohitmauryax <76580832+rohitmauryax@users.noreply.github.com>
2023-07-30 03:01:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoDB = require("./db");
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://foodhub123.netlify.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With, Content-Type, Accept"
